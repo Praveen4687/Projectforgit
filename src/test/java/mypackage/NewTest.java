683f4ecc9d0b1e92a8e68ba0e075e7c6a3c0f40f
Praveen4687 <151002794+Praveen4687@users.noreply.github.com>
2023-11-16 13:12:36 +0530

    xf

--- src/test/java/mypackage/NewTest.java
+++ src/test/java/mypackage/NewTest.java
@@ -7,7 +7,9 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void LoginTest() {
-	  WebDriver driver=new ChromeDriver()
+	  WebDriver driver=new ChromeDriver();
+	  int a=0,b=20,c
+			  c=a+b
 	  
   }
 }
